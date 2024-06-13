87b291fb2caa25e3fc017a9776750ece5e86a155
Nishant <nishantchaudhary842@gmail.com>
2024-06-13 21:36:28 +0545

    refactor

--- book_review/urls.py
+++ book_review/urls.py
@@ -9,16 +9,16 @@ urlpatterns = [
     
     
     path('login/',  auth.login, name='login'),
-    path('user/books',  GetLoginUserBook.as_view(), name='user_books'),
-    path('user/books/delete/<int:book_id>',  DeleteBook.as_view(), name='delete_book'),
+    path('user/register',  createUser.as_view(), name='create_user'),
     
     # user
     path('user/',  allUser.as_view(), name='user'),
+    path('user/<int:id>',  singleUser.as_view(), name='single_user'),
+    
     #book
     path('books/<int:book_id>/update', UpdateBookView.as_view(), name='update-book'),
-
-    path('user/<int:id>',  singleUser.as_view(), name='single_user'),
-    path('user/register',  createUser.as_view(), name='create_user'),
+    path('user/books/delete/<int:book_id>',  DeleteBook.as_view(), name='delete_book'),
+    path('user/books',  GetLoginUserBook.as_view(), name='user_books'),
     path('book/create',  CreateBookView.as_view(), name='create_book'),
     path('book/<int:id>',  singleBook.as_view(), name='get_single_book'),
     path('book/',  AllBooks.as_view(), name='all_book'),
